5bbb245b64ed612e9a37146dbda3fc954210a84f
Arimototoku <sonne@i.ua>
2018-11-25 19:37:52 +0200

    Add files via upload

--- Network.java
+++ Network.java
@@ -5,7 +5,7 @@ public class Network {
     private Phone[] phones = new Phone[5];
     private int index = 0;
 
-    public Network (){
+    public Network() {
     }
 
     public Phone[] getPhones() {
@@ -14,13 +14,18 @@ public class Network {
 
     /**
      * Method  add phone number in array
+     *
      * @param phone
      */
-    public void addNewNumber (Phone phone){
+    public void addNewNumber(Phone phone) {
         for (int i = 0; i < phones.length; i++) {
-            if (phones[i].getNumber() == phone.getNumber()){
-                System.out.println("This number is registered already");
-                return;
+            if (phones[i] != null) {
+                if (phones[i].getNumber() == phone.getNumber()) {
+                    System.out.println("This number is registered already");
+                    return;
+                }
+            } else {
+                break;
             }
         }
         phones[index] = phone;
